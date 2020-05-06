240a2cbb6a50247ee993e32f17aebc9d41d3249c
rabah babaci <4rabah@gmail.com>
2020-05-06 11:34:43 -0700

    make the server listen on port 5000

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
