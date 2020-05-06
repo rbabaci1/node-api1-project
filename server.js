0c5f478d75344f7937430eba8d457febc93f60a5
rabah babaci <4rabah@gmail.com>
2020-05-06 11:36:57 -0700

    test the server on a get request to the users

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const shortid = require('shortid');
 const server = express();
 const PORT = 5000;
 
+const users = [];
+
+server.get('/users', (req, res) => {
+  res.send('test');
+});
+
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
