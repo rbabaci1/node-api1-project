54d2049e85c1da2fb5cda04c39065dfaf80a409e
rabah babaci <4rabah@gmail.com>
2020-05-06 15:26:15 -0700

    implement the post method on the server

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ const users = [];
 server.use(express.json());
 
 server.post('/api/users', (req, res) => {
-  const reqs = req;
+  const user = req.body;
 
-  console.log(reqs);
+  if ('name' in user && 'bio' in user) {
+    user.id = shortid.generate();
+    users.push(user);
+    res.status(201).json(user);
+  } else {
+    res
+      .status(400)
+      .send({ errorMessage: 'Please provide name and bio for the user.' });
+  }
 });
 
 server.listen(PORT, () => {
