3bdb772aaf0bd7970d613ebb665b4dccb887ab3e
rabah babaci <4rabah@gmail.com>
2020-05-07 12:17:09 -0700

    implement the PATCH request handler on the server

--- server.js
+++ server.js
@@ -57,6 +57,29 @@ server.delete('/api/users/:id', (req, res) => {
   }
 });
 
+server.patch('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  if ('name' in changes && 'bio' in changes) {
+    const found = users.find((user) => user.id === id);
+
+    if (found) {
+      Object.assign(found, changes);
+
+      res.status(200).json(found);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist.' });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  }
+});
+
 server.put('/api/users/:id', (req, res) => {
   const { id } = req.params;
   const updatedUser = req.body;
