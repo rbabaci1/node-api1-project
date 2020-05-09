8dec53f48eb825b25020f04038719d8be1cbd2a8
rabah babaci <4rabah@gmail.com>
2020-05-09 10:08:11 -0700

    stash

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const userRoutes = require('./users/userRoutes');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
